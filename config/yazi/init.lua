6c4928499b642bfe5c1fd5d8a5c61350d436e43b
Dmitry Karpachev <dmitry.karpachev@hotmail.com>
2025-07-20 14:43:08 +0700

    yazi deleted

--- config/yazi/init.lua
+++
@@ -1,9 +0,0 @@
-require("full-border"):setup()
-require("git"):setup()
-
-require("gvfs"):setup({
-  save_path = os.getenv("HOME") .. "/.config/yazi/gvfs.private",
-
-  save_password_autoconfirm = true,
-})
-
