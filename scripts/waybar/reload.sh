ef08aa3d2069f2ac29067c59694c5cec5a13988e
Dmitry Karpachev <dmitry.karpachev@hotmail.com>
2025-08-01 16:56:29 +0700

    configs updated

--- scripts/waybar/reload.sh
+++ scripts/waybar/reload.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
 
-CONFIG_PATH="$HOME/.config/waybar/config.jsonc"
+CONFIG_DIR="$HOME/.config/waybar"
+
+CONFIG_SYMLINK="$CONFIG_DIR/config.jsonc"
+STYLE_SYMLINK="$CONFIG_DIR/style.css"
+
+CONFIG_1="$CONFIG_DIR/main/config_main.jsonc"
+STYLE_1="$CONFIG_DIR/main/style_main.css"
+
+if [[ ! -L "${CONFIG_SYMLINK}" ]]; then
+    ln -s "${CONFIG_1}" "${CONFIG_SYMLINK}"
+fi
+
+if [[ ! -L "${STYLE_SYMLINK}" ]]; then
+    ln -s "${STYLE_1}" "${STYLE_SYMLINK}"
+fi
 
 killall waybar
-waybar -c "${CONFIG_PATH}" &
+waybar -c "${CONFIG_SYMLINK}" &
 
 
