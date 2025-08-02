f16975c939ae4006123d789b5d06f72c52c8788d
Dmitry Karpachev <dmitry.karpachev@hotmail.com>
2025-08-03 00:11:21 +1000

    screenshot and color scripts updated

--- scripts/screenshot/print_fscreen.sh
+++ scripts/screenshot/print_fscreen.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
-grim - | wl-copy -t image/png && notify-send "Screenshot" "Full screen copied to clipboard" -i screenshot
+screenshot_file="/tmp/screenshot.png"
+thumb_dir="$HOME/.cache/thumbnails/cliphist"
+thumb_file="$thumb_dir/screenshot_$(date +%Y-%m-%d_%H-%M-%S).png"
+
+mkdir -p "$thumb_dir"
+
+grim - | tee "$screenshot_file" | wl-copy -t image/png
+
+convert "$screenshot_file" -thumbnail 200 "$thumb_file"
+
+notify-send "Screenshot" "Full screen copied to clipboard" -i "$thumb_file"
+
+rm -f "$screenshot_file"
 
--- scripts/screenshot/print_fscreen_tf.sh
+++ scripts/screenshot/print_fscreen_tf.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-grim ~/Pictures/Screenshots/screenshot_$(date +%Y-%m-%d_%H-%M-%S).png && notify-send "Screenshot" "Full screen saved in Screenshots dir" -i screenshot
+filepath="$HOME/Pictures/Screenshots/screenshot_$(date +%Y-%m-%d_%H-%M-%S).png"
+thumb_file="/tmp/screenshot_thumb.png"
+
+grim "$filepath"
+
+convert "$filepath" -thumbnail 200 "$thumb_file"
+
+notify-send "Screenshot" "Full screen saved in \"Screenshots\" dir" -i "$thumb_file"
+
+rm -f "$thumb_file"
 
