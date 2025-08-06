d9635beb8f1857b37e806fde74223be3a4a2e711
Dmitry Karpachev <dmitry.karpachev@hotmail.com>
2025-08-07 00:27:06 +1000

    added neovim config

---
+++ config/nvim/lua/vim-options.lua
@@ -0,0 +1,14 @@
+--vim settings
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+
+vim.cmd("set smarttab")
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set autoindent")
+vim.cmd("set smartindent")
+vim.cmd("syntax on")
+vim.cmd("set so=30")
+
