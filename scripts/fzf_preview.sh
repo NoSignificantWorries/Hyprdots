aeffb9a5ca86d298e8fb8c91c14958905ad7b409
Dmitry Karpachev <dmitry.karpachev@hotmail.com>
2025-08-07 00:07:10 +1000

    modifyed fzf preview script | deleted eww configs

--- scripts/fzf_preview.sh
+++ scripts/fzf_preview.sh
@@ -2,16 +2,12 @@
 
 file="$1"
 
-clear
-
 if [[ -d "$file" ]]; then
     tree -C "$file"
-elif [[ "$file" == *.png || "$file" == *.jpg ]]; then
-    jp2a --color-depth=24 --width=$((FZF_PREVIEW_COLUMNS)) "$file"
-    # timg -g$((FZF_PREVIEW_COLUMNS))x$((FZF_PREVIEW_LINES)) "$file"
-elif [[ "$file" == *.ipynb ]]; then
-    rich "$file"
 else
-    bat --color=always --style=header,grid "$file"
+    case "$file" in
+        *.png|*.jpg|*.jpeg|*.svg|*.gif) timg -g$((FZF_PREVIEW_COLUMNS))x$((FZF_PREVIEW_LINES)) "$file" ;;
+        *) rich "$file" ;;
+    esac
 fi
 
