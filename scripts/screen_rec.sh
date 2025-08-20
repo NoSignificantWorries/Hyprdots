8d99b78cf5a9cd49f63872438efcec53704baddc
NoSignificantWorries <dmitry.karpachev@hotmail.com>
2025-08-20 19:50:01 +0700

    Added screen recorder
    Changed screen capture script:
    - added screen recording
    - added screen recording status
    - added wyabar module for recorder
    
    So changed symlinks list and hyprland keymappings

--- scripts/screen_rec.sh
+++ scripts/screen_rec.sh
@@ -9,6 +9,7 @@ mkdir -p "$screenshot_dir"
 mkdir -p "$recording_dir"
 
 screenshot_file="/tmp/screenshot.png"
+recorder_pid_file="/tmp/wf-recorder.pid"
 
 screen_thumb="$thumb_dir/screenshot_$(date +%Y-%m-%d_%H-%M-%S).png"
 filepath="$screenshot_dir/screenshot_$(date +%Y-%m-%d_%H-%M-%S).png"
@@ -45,24 +46,54 @@ case "$ACTION" in
         rm -f "$screen_thumb"
         ;;
     "record-full-start")
+        if [ -f "$recorder_pid_file" ]; then
+            notify-send "Recording" "ERROR: Screen recorder already working" -i "$HOME/.icons/recording.png"
+            exit 0
+        fi
         wf-recorder -f "$recpath" &
-        echo $! > /tmp/wf-recorder.pid
-        notify-send "Запись" "Начата запись видео"
+        echo $! > "$recorder_pid_file"
+        notify-send "Recording" "Screen recording started" -i "$HOME/.icons/recording.png"
         ;;
     "record-area-start")
-        scale=$(hyprctl monitors | awk '/scale/ {print $2}')
-        area=$(slurp -b '00000075' -c '#b4befe' -d)
+        if [ -f "$recorder_pid_file" ]; then
+            notify-send "Recording" "ERROR: Screen recorder already working" -i "$HOME/.icons/recording.png"
+            exit 0
+        fi
 
-        echo "$area"
+        area=$(slurp -b '00000075' -c '#b4befe' -d)
+        scale=$(hyprctl monitors | grep scale | awk '{print $2}')
+        read x y width height <<< $(echo "$area" | sed 's/[,x ]/ /g')
+
+        Sx=$(printf "%.0f" "$(bc <<< "$x * $scale")")
+        Sy=$(printf "%.0f" "$(bc <<< "$y * $scale")")
+        Swidth=$(printf "%.0f" "$(bc <<< "$width * $scale")")
+        Sheight=$(printf "%.0f" "$(bc <<< "$height * $scale")")
+
+        scaled_area="$Sx,$Sy ${Swidth}x${Sheight}"
+
         wf-recorder -g "$scaled_area" -f "$recpath" &
-        echo $! > /tmp/wf-recorder.pid
-        notify-send "Запись" "Начата запись видео"
+        echo $! > "$recorder_pid_file"
+        notify-send "Recording" "Area recording started" -i "$HOME/.icons/recording.png"
         ;;
     "record-stop")
+        if [ ! -f "$recorder_pid_file" ]; then
+            exit 0
+        fi
+
         sleep 0.5
-        kill -INT $(cat /tmp/wf-recorder.pid)
-        rm /tmp/wf-recorder.pid
-        notify-send "Запись" "Запись видео остановлена"
+        kill -INT $(cat "$recorder_pid_file")
+        rm "$recorder_pid_file"
+        notify-send "Recording" "Recording stopped" -i "$HOME/.icons/recording.png"
+        ;;
+    "record-status")
+        if [ ! -f "$recorder_pid_file" ]; then
+            echo '{"text":"<span size=\"16000\" weight=\"bold\" color=\"#ff0000\">◉</span>", "tooltip":"Stopped"}'
+            exit 0
+        fi
+
+        PID=$(cat "$recorder_pid_file")
+        time=$(ps -p $(cat /tmp/wf-recorder.pid) -o etime= | awk '{print $1}')
+        echo "{\"text\":\"<span size=\\\"12000\\\"><span weight=\\\"bold\\\" color=\\\"#ff0000\\\">◻</span> $time</span>\", \"tooltip\":\"Working\"}"
         ;;
     *)
         echo "ERROR: Unknown option"
