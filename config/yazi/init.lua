a896153f0d46543d6ec8f8552bd8af5511cd7479
NoSignificantWorries <dmitry.karpachev@hotmail.com>
2025-11-29 06:14:10 +0000

    Updates from pc

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -4,8 +4,3 @@ require("full-border"):setup {
 	type = ui.Border.ROUNDED,
 }
 
-require("gvfs"):setup({
-  save_path = os.getenv("HOME") .. "/.config/yazi/gvfs.private",
-  save_password_autoconfirm = true,
-})
-
--- config/yazi/install_plugins.sh
+++ config/yazi/install_plugins.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 ya pkg add yazi-rs/plugins:full-border
-ya pkg add boydaihungst/gvfs
 ya pkg add boydaihungst/mediainfo
 ya pkg add ndtoan96/ouch
 ya pkg add AnirudhG07/nbpreview
@@ -10,5 +9,4 @@ ya pkg add yazi-rs/plugins:chmod
 ya pack -a Lil-Dank/lazygit
 ya pkg add pirafrank/what-size
 ya pkg add yazi-rs/plugins:git
-ya pack -a Reledia/hexyl
 
